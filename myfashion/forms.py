b8974b705d016d7c27309fe8c7c96d72ec4187d7
sharon0812 <anyangosharon@gmail.com>
2021-02-05 03:11:28 +0300

    add registration form

--- myfashion/forms.py
+++ myfashion/forms.py
@@ -2,11 +2,10 @@ from django import forms
 from .models import Post
 
         
-# class AvisForm(forms.ModelForm):
-#     class Meta:
-#         model = Avis
-#         fields = ['note']
-#         widgets = {'note': forms.NumberInput(attrs={'class': 'Stars'})}
-#         labels = {'note': 'Note /5'}
-#         fields = ['commentaire']
-#         widgets = {'commentaire': forms.Textarea(attrs={'class': 'form-control'})}
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post 
+        fields = ['image', 'title', 'desc', 'link'] 
+
+        
+
--- myfashion/urls.py
+++ myfashion/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('',welcome, name='home'),
     path('search/', views.search_results, name='search_results'),
     path('about/', views.about, name='about'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     
 ]
 if settings.DEBUG:
