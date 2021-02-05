33d3449d37016254d18a83081984d0fa1548d396
sharon0812 <anyangosharon@gmail.com>
2021-02-05 03:42:23 +0300

    add logout form

--- fashion/urls.py
+++ fashion/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myfashion.urls')),
     path('ratings/', include('star_ratings.urls', namespace='ratings')),
+    path("logout", views.logout_then_login, name= "logout") 
     
 ]
--- myfashion/urls.py
+++ myfashion/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
